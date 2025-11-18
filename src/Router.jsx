48c44eae62b51affc7406c2dc6e3dfb50b09ad09
Chris Bailey <kawipro32@gmail.com>
2025-11-18 00:42:53 -0800

    Add gh pages reqs

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,14 +1,16 @@
 // 2025 SIBIL LLC. All rights reserved.
 // SIBIL Confidential
 
-import { createBrowserRouter } from "react-router-dom";
+// import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import Root from "./root.jsx";
 import { HomePage } from "./pages/marketing/HomePage.jsx";
 import { PricingPage } from "./pages/marketing/PricingPage.jsx";
 import { AboutPage } from "./pages/marketing/AboutPage.jsx";
 import { FeaturePage } from "./pages/marketing/FeaturePage.jsx";
 
-export const Router = createBrowserRouter([
+// export const Router = createBrowserRouter([
+    export const Router = createHashRouter([
   {
     id: "root",
     path: "/",
